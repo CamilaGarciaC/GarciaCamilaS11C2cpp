da28c29c2220afb36f4275e6015c93d1eb7d48df
Camila  Garcia C <camilagarciac@Air-de-Camila-6.uniandes.edu.co>
2019-04-04 13:53:53 -0500

    codigo

---
+++ primos.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int funcion(int n, int m);
+
+int main(){
+	funcion(int n, int m);
+	return 0;
+}
+
+int funcion(int n, int m)
+{
+	z=m*n;
+	for(int i=n;i<=m;i++)
+		for(int j=2, j<=z, j++)
+			if(j==i)
+				j = j++;
+			else(i%j==0)
+				primo = i;
+		std::cout << primo <<std::endl;	
+}
